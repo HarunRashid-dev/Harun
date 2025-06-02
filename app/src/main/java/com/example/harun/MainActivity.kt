33b328d550114c5461fdcaf816cc5d20a9fbc94e
M Harun Rashid <harunrashid9159@gmail.com>
2025-06-02 08:36:53 +0530

    Size

--- app/src/main/java/com/example/harun/MainActivity.kt
+++ app/src/main/java/com/example/harun/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +28,7 @@ fun HelloWorld() {
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ){
-        Text(text = "Hello World!", color = Color.Blue)
+        Text(text = "Hello World!", color = Color.Blue, fontSize = 40.sp)
     }
 
 }
