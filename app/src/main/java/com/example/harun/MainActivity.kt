97f4e7b9d38e3d24bd222020da09ad1423d04f47
M Harun Rashid <harunrashid9159@gmail.com>
2025-06-02 07:31:23 +0530

    Center

--- app/src/main/java/com/example/harun/MainActivity.kt
+++ app/src/main/java/com/example/harun/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.harun
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +21,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HelloWorld() {
-    Text(text = "Hello World")
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = "Hello World!")
+    }
+
 }
